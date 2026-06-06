df7141f3b23ac227853018454731435704f89b21
Tauan-Ray <tauanray995@gmail.com>
2026-06-05 22:07:58 -0300

    feat: add user repository contract

---
+++ src/main/java/br/com/tauan/agendamento/user/domain/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package br.com.tauan.agendamento.user.domain.repository;
+
+import br.com.tauan.agendamento.user.domain.entity.User;
+import br.com.tauan.agendamento.user.domain.valueobject.Email;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    List<User> listAll();
+    Optional<User> findById(UUID id);
+    Optional<User> findByEmail(Email email);
+    User save(User user);
+}
