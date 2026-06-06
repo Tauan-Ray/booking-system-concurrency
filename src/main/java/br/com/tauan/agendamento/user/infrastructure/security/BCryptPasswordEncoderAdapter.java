a8cced70a96b1eaec0480e2aa5f867112a1b90c1
Tauan-Ray <tauanray995@gmail.com>
2026-06-05 23:26:36 -0300

    feat: add password encoder adapter

---
+++ src/main/java/br/com/tauan/agendamento/user/infrastructure/security/BCryptPasswordEncoderAdapter.java
@@ -0,0 +1,21 @@
+package br.com.tauan.agendamento.user.infrastructure.security;
+
+import br.com.tauan.agendamento.user.application.contract.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptPasswordEncoderAdapter implements PasswordEncoder {
+
+    private final org.springframework.security.crypto.password.PasswordEncoder encoder =
+            new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+
+    @Override
+    public String encode(String rawPassword) {
+        return encoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword, encodedPassword);
+    }
+}
