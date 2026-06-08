7438b7950369c6bc2ae1784a3e438384407c3933
Tauan-Ray <tauanray995@gmail.com>
2026-06-08 20:35:54 -0300

    feat: restrict user details access to owner or admin

--- src/main/java/br/com/tauan/agendamento/user/application/usecase/GetUserByIdUseCase.java
+++ src/main/java/br/com/tauan/agendamento/user/application/usecase/GetUserByIdUseCase.java
@@ -1,5 +1,7 @@
 package br.com.tauan.agendamento.user.application.usecase;
 
+import br.com.tauan.agendamento.shared.application.contract.AuthenticatedUserProvider;
+import br.com.tauan.agendamento.shared.domain.exception.ForbiddenException;
 import br.com.tauan.agendamento.user.application.exception.UserNotFoundException;
 import br.com.tauan.agendamento.user.domain.entity.User;
 import br.com.tauan.agendamento.user.domain.repository.UserRepository;
@@ -11,10 +13,22 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class GetUserByIdUseCase {
+
     private final UserRepository userRepository;
+    private final AuthenticatedUserProvider auth;
 
     public User execute(UUID id) {
-        return userRepository.findById(id)
+        User user = userRepository.findById(id)
                 .orElseThrow(UserNotFoundException::new);
+
+        UUID requesterId = auth.getUserId();
+        boolean isOwner = requesterId.equals(id);
+        boolean isAdmin = auth.hasRole("ADMIN");
+
+        if (!isOwner && !isAdmin) {
+            throw new ForbiddenException();
+        }
+
+        return user;
     }
 }
