d55d85e5866b662463c6e777ecef3b4e0477d293
Tauan-Ray <tauanray995@gmail.com>
2026-06-08 20:53:23 -0300

    test: remove unused testcontainers configuration

--- src/test/java/br/com/tauan/agendamento/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package br.com.tauan.agendamento;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
