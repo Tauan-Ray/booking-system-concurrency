8b22a412bfe2852c19190c98c0e1b3eedf7e37c8
Tauan-Ray <tauanray995@gmail.com>
2026-06-09 21:57:22 -0300

    feat: handle invalid request body and invalid params errors in global exception handler

--- src/main/java/br/com/tauan/agendamento/shared/presentation/advice/GlobalExceptionHandler.java
+++ src/main/java/br/com/tauan/agendamento/shared/presentation/advice/GlobalExceptionHandler.java
@@ -4,10 +4,14 @@ import br.com.tauan.agendamento.shared.domain.exception.DomainException;
 import br.com.tauan.agendamento.shared.presentation.dto.response.ApiResponse;
 import br.com.tauan.agendamento.shared.presentation.dto.response.ErrorResponse;
 import br.com.tauan.agendamento.shared.presentation.dto.response.FieldErrorResponse;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 
@@ -39,6 +43,66 @@ public class GlobalExceptionHandler {
                         err.getDefaultMessage()
                 ))
                 .toList();
+
+        ErrorResponse error = new ErrorResponse(
+                "Validation failed",
+                400,
+                "VALIDATION_ERROR",
+                fieldErrors
+        );
+
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error(error));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<Object>> handleInvalidFormat(
+            HttpMessageNotReadableException ex
+    ) {
+
+        List<FieldErrorResponse> fieldErrors = List.of();
+
+        if (ex.getCause() instanceof InvalidFormatException invalidFormat) {
+
+            String field = invalidFormat.getPath()
+                    .stream()
+                    .findFirst()
+                    .map(JsonMappingException.Reference::getFieldName)
+                    .orElse("unknown");
+
+
+            fieldErrors = List.of(
+                    new FieldErrorResponse(
+                            field,
+                            "Invalid value"
+                    )
+            );
+        }
+
+        ErrorResponse error = new ErrorResponse(
+                "Validation failed",
+                400,
+                "VALIDATION_ERROR",
+                fieldErrors
+        );
+
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error(error));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiResponse<Object>> handleTypeMismatch(
+            MethodArgumentTypeMismatchException ex
+    ) {
+
+        List<FieldErrorResponse> fieldErrors = List.of(
+                new FieldErrorResponse(
+                        ex.getName(),
+                        "Invalid value"
+                )
+        );
 
         ErrorResponse error = new ErrorResponse(
                 "Validation failed",
